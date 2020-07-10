d3a5b2127d6d89292c2410086630dd85e3327fd7
Nivas Muthu <nivasmuthu452@gmail.com>
2020-07-10 15:25:48 +0530

    added more info to the memory structure

--- top/memory.go
+++ top/memory.go
@@ -26,6 +26,7 @@ type Memory struct {
 	SwapTotal    int64
 	SwapFree     int64
 	Dirty        int64
+	Writeback    int64
 	AnonPages    int64
 	Mapped       int64
 	SharedMem    int64
@@ -63,12 +64,48 @@ func (top *SystemTopStruct) GetMemoryInfo() error {
 			return err
 		}
 		switch {
-		case strings.Contains(v, "MemTotal:"):
+		case strings.Compare(tempList[0], "MemTotal:") == 0:
 			tempMemory.MemTotal = temp
-		case strings.Contains(v, "MemFree:"):
+		case strings.Compare(tempList[0], "MemFree:") == 0:
 			tempMemory.MemFree = temp
-		case strings.Contains(v, "MemAvailable:"):
+		case strings.Compare(tempList[0], "MemAvailable:") == 0:
 			tempMemory.MemAvail = temp
+		case strings.Compare(tempList[0], "Buffers:") == 0:
+			tempMemory.Buffers = temp
+		case strings.Compare(tempList[0], "Cached:") == 0:
+			tempMemory.Cached = temp
+		case strings.Compare(tempList[0], "SwapCached:") == 0:
+			tempMemory.SwapCached = temp
+		case strings.Compare(tempList[0], "Active:") == 0:
+			tempMemory.Active = temp
+		case strings.Compare(tempList[0], "Inactive:") == 0:
+			tempMemory.InActive = temp
+		case strings.Compare(tempList[0], "Active(anon):") == 0:
+			tempMemory.ActiveAnon = temp
+		case strings.Compare(tempList[0], "Inactive(anon):") == 0:
+			tempMemory.InActiveAnon = temp
+		case strings.Compare(tempList[0], "Active(file):") == 0:
+			tempMemory.ActiveFile = temp
+		case strings.Compare(tempList[0], "Inactive(file):") == 0:
+			tempMemory.InActiveFile = temp
+		case strings.Compare(tempList[0], "Unevictable:") == 0:
+			tempMemory.Unevictable = temp
+		case strings.Compare(tempList[0], "Mlocked:") == 0:
+			tempMemory.Mlocked = temp
+		case strings.Compare(tempList[0], "SwapTotal:") == 0:
+			tempMemory.InActiveFile = temp
+		case strings.Compare(tempList[0], "SwapFree:") == 0:
+			tempMemory.SwapFree = temp
+		case strings.Compare(tempList[0], "Dirty:") == 0:
+			tempMemory.Dirty = temp
+		case strings.Compare(tempList[0], "Writeback:") == 0:
+			tempMemory.Writeback = temp
+		case strings.Compare(tempList[0], "AnonPages:") == 0:
+			tempMemory.AnonPages = temp
+		case strings.Compare(tempList[0], "Mapped:") == 0:
+			tempMemory.Mapped = temp
+		case strings.Compare(tempList[0], "Shmem:") == 0:
+			tempMemory.SharedMem = temp
 		}
 	}
 	top.memory = tempMemory
